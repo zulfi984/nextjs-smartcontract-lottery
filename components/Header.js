418043367187b6d2667d3a09b94b6bc8184dda01
zulfi984 <zulfizee1@gmail.com>
2022-08-07 11:01:27 +0530

    added tailwind css styling

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,12 @@ import React from "react"
 import { ConnectButton } from "web3uikit"
 const Header = () => {
     return (
-        <div>
-            <ConnectButton moralisAuth={false} />
-        </div>
+        <nav className="p-5 border-b-2 flex flex-row">
+            <h1 className="py-4 px-4 font-bold text-3xl"> Decentralized Lottery</h1>
+            <div className="ml-auto py-2 px-4">
+                <ConnectButton moralisAuth={false} />
+            </div>
+        </nav>
     )
 }
 
--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,37 +1,109 @@
-import React from "react"
+import { contractAddresses, abi } from "../constants/index"
 import { useMoralis, useWeb3Contract } from "react-moralis"
-import { abi, contractAddresses } from "../constants"
-import { useEffect } from "react"
-const LotteryEntrance = () => {
-    const { chainId, isWeb3Enabled } = useMoralis()
-    //console.log(parseInt(chainId))
+import { useEffect, useState } from "react"
+import { ethers } from "ethers"
+import { useNotification } from "web3uikit"
+
+export default function LotteryEntrance() {
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    const chainId = parseInt(chainIdHex)
     const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
-    let entranceFee = ""
-    const { runContractFunction: enterRaffle } = useWeb3Contract({
+    const [entranceFee, setEntranceFee] = useState("0") //entranceFee= variable we call to see the get the entrancefee, set entrance fee is the
+    //function we call to update/set that entrance fee. Its parameter is the return value from our getEntranceFee function
+
+    const [numPlayers, setNumPlayers] = useState("0")
+    const [recentWinner, setRecentWinner] = useState("0")
+    const dispatch = useNotification()
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress, // specify the networkId
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    const {
+        runContractFunction: enterRaffle,
+        isLoading,
+        isFetching,
+    } = useWeb3Contract({
         abi: abi,
         contractAddress: raffleAddress,
         functionName: "enterRaffle",
         params: {},
-        // msgValue:?,
+        msgValue: entranceFee,
     })
-    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+    const { runContractFunction: getNumberOfPlayers } = useWeb3Contract({
         abi: abi,
-        contractAddress: raffleAddress,
-        functionName: "getEntranceFee",
+        contractAddress: raffleAddress, // specify the networkId
+        functionName: "getNumberOfPlayers",
         params: {},
-        // msgValue:?,
+    })
+    const { runContractFunction: getRecentWinner } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress, // specify the networkId
+        functionName: "getRecentWinner",
+        params: {},
     })
     async function updateUI() {
-        if (isWeb3Enabled) {
-            const entranceFeeFromCall = (await getEntranceFee()).toString()
-            console.log(entranceFeeFromCall)
-        }
+        const entranceFeeFromCall = (await getEntranceFee()).toString()
+        const numPlayersFromCall = (await getNumberOfPlayers()).toString()
+        const recentWinnerFromCall = (await getRecentWinner()).toString()
+        setEntranceFee(entranceFeeFromCall)
+        setNumPlayers(numPlayersFromCall)
+        setRecentWinner(recentWinnerFromCall)
+        console.log(entranceFee)
     }
+    //here we are using useEffect to get the entrancefee from the contract. Because getEntranceFee is an async function we need to make a async function
+    //that is called when web3 is enable/we have access to our contract. Gets entrance as soon as the web loads
     useEffect(() => {
-        // await function wont work in useEffect
-
-        updateUI()
+        if (isWeb3Enabled) {
+            updateUI()
+        }
     }, [isWeb3Enabled])
-}
 
-export default LotteryEntrance
+    const handleSuccess = async function (tx) {
+        await tx.wait(1)
+        handleNewNotification(tx)
+        updateUI()
+    }
+    const handleNewNotification = function () {
+        dispatch({
+            type: "info",
+            message: "Transaction Complete",
+            title: "Tx Notifcation",
+            position: "topR",
+            icon: "bell",
+        })
+    }
+
+    return (
+        <div>
+            Hi from lottery entrance
+            {raffleAddress ? (
+                <div>
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl ml-auto"
+                        onClick={async function () {
+                            await enterRaffle({
+                                onSuccess: handleSuccess,
+                                onError: (err) => console.log(err),
+                            })
+                        }}
+                        disabled={isLoading || isFetching}
+                    >
+                        {isLoading || isFetching ? (
+                            <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+                        ) : (
+                            "Enter Raffle"
+                        )}
+                    </button>
+                    <div>Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH</div>
+                    <div>The current number of players is: {numPlayers}</div>
+                    <div>The most previous winner was: {recentWinner}</div>
+                </div>
+            ) : (
+                <div>No Raffle Address Detected !!!</div>
+            )}
+        </div>
+    )
+}
